cddce1b7eaf4805de54851eb0e4d759d4f3955da
Lyzu <dev@lyzu.net>
2017-02-05 21:25:46 +0100

    - fixed a bug not listening to events correctly. The addon should now work if you reload ui while in a raidgroup - fixed a bug where syncing would not work with real id friends. The addon should now reliably sync if someone reloads their ui - added the '/sd status' command which lets you easily see if StayDead is enabled

--- StayDead.lua
+++ StayDead.lua
@@ -9,6 +9,8 @@ local sync = CreateFrame("Frame", "StayDead_sync")
 -- register events
 events:RegisterEvent("PLAYER_LOGIN")
 events:RegisterEvent("GROUP_JOINED")
+events:RegisterEvent("GROUP_LEFT")
+events:RegisterEvent("PLAYER_DEAD")
 sync:RegisterEvent("CHAT_MSG_ADDON")
 
 -- disable addon by default
@@ -34,7 +36,6 @@ function StayDead_fetch()
                 SendAddonMessage(addon_prefix, "fetch:" .. UnitName("player"), "WHISPER", name);
             end
         end
-
     -- party
     elseif (IsInGroup(LE_PARTY_CATEGORY_HOME)) then
         for i=1,GetNumGroupMembers(),1 do
@@ -50,60 +51,39 @@ events:SetScript("OnEvent", function(self, event, arg1)
     -- fire on death
     if (event == "PLAYER_DEAD") then
         StayDead_Status();
-
     elseif (event == "GROUP_JOINED") then
-        events:UnregisterEvent("GROUP_JOINED")
-        events:RegisterEvent("GROUP_LEFT")
-        events:RegisterEvent("PLAYER_DEAD")
         StayDead_fetch()
-    
     elseif (event == "GROUP_LEFT") and (status == "on") then
-        events:RegisterEvent("GROUP_JOINED")
-        events:UnregisterEvent("GROUP_LEFT")
-        events:UnregisterEvent("PLAYER_DEAD")
-        
         print("|cFF8753efStayDead|r disabled");
         status = "off"
-
     elseif (event == "PLAYER_LOGIN") then
         StayDead_fetch()
     end
 end)
 
 -- sync handling
-sync:SetScript("OnEvent", function(self, event, prefix, message, channel, fullsender, sender)
+sync:SetScript("OnEvent", function(self, event, prefix, message, channel, fullsender)
     if (event == "CHAT_MSG_ADDON") and (prefix == addon_prefix) then
+        local sender = string.match(fullsender, "(%a*)-.*")
         local action, message = string.match(message, "(%a*):(.*)")
-    
+
         -- receiving
         if (action == "sync") then
             if (UnitIsGroupLeader(sender)) then
                 -- updating status
                 if (message == "StayDead_on") and (status == "off") then
-                    print("|cFF8753efStayDead|r activated");
+                    print("|cFF8753efStayDead|r enabled");
                     status = "on";
-                
                 elseif (message == "StayDead_off") and (status == "on") then
                     print("|cFF8753efStayDead|r disabled");
                     status = "off";
                 end
             end
-    
         -- fetching
         elseif (action == "fetch") then
-            if (UnitInParty(sender)) then
-                -- if sender is not set
-                if (sender == nil) then
-                    local sender = string.match(sender, "(%a*)-.*")                
-                end
-                
-                if (sender ~= nil) then
-                    print(string.format("Could not update. Message: %s", message));
-                else
-                    SendAddonMessage(addon_prefix, "sync:" .. addon_prefix .. "_" .. status, "WHISPER", sender);
-                end
+            if (sender ~= nil) and (UnitInParty(sender))then
+                SendAddonMessage(addon_prefix, "sync:" .. addon_prefix .. "_" .. status, "WHISPER", sender);
             end
-
         elseif (action == "res") then
             if (UnitIsGroupLeader(sender)) then
                 RepopMe();
@@ -114,18 +94,24 @@ end)
 
 -- slash handler
 local function handler(msg, editbox)
-    -- sync to others
-    if (UnitIsGroupLeader("player")) then
-        if (msg == "on") or (msg == "off") then
-            SendAddonMessage(addon_prefix, "sync:" .. addon_prefix .. "_" .. msg, "RAID");
-
-        elseif (msg == "res") then
-            SendAddonMessage(addon_prefix, "res:all", "RAID");
+    if (msg == "status") then
+        if (status == "on") then
+            print("|cFF8753efStayDead|r is enabled");
+        elseif (status == "off") then
+            print("|cFF8753efStayDead|r is disabled");
         end
-
-    -- restricted
     else
-        print("You need to be leader to use this function.")
+        -- sync to others
+        if (UnitIsGroupLeader("player")) then
+            if (msg == "on") or (msg == "off") then
+                SendAddonMessage(addon_prefix, "sync:" .. addon_prefix .. "_" .. msg, "RAID");
+            elseif (msg == "res") then
+                SendAddonMessage(addon_prefix, "res:all", "RAID");
+            end
+        -- restricted
+        else
+            print("You need to be leader to use this function.")
+        end
     end
 end
 
